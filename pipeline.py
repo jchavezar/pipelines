1369ae9cd00619034ffffbb8c0c0b60fc3e6be2a
jchavezar <jchavezar@gmail.com>
2021-11-24 15:25:40 -0500

    test

--- pipeline.py
+++ pipeline.py
@@ -6,22 +6,22 @@ from typing import NamedTuple
 from kfp.v2.dsl import pipeline
 from kfp.v2.dsl import component
 from kfp.v2 import compiler
+import time
+
+timestr = time.strftime("%H%M")
+pipeline_name='pipe-{}'.format(timestr)
+compiled_pipe='pipeline_1'
 
 @component
-def concat(a: str, b: str) -> str:
+def op(a: float, b: float) -> float:
     return a+b
 
-@component
-def reverse(a: str) -> NamedTuple("outputs", [("before", str), ("after", str)]):
-    return a, a[::-1]
-
-@pipeline(name="basic-pipeline", description="A simple intro pipeline",
+@pipeline(name="{}".format(pipeline_name), description="A simple intro pipeline",
           pipeline_root='gs://tron-302502-bucket/basic-pipe')
 def basic_pipeline(a: str='testing1', b: str='testing2'):
-    concat_task = concat(a,b)
-    reverse_task = reverse(concat_task.output)
+    op_task = op(a,b)
     
 if __name__ == '__main__':
     compiler.Compiler().compile(
-        pipeline_func=basic_pipeline, package_path="basic_pipeline_1.json"
+        pipeline_func=basic_pipeline, package_path="{}.json".format(compiled_pipe)
     )
