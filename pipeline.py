445c8d05f2f3e1a9a3a1c63f67f70197bffe3d6a
jchavezar <jchavezar@gmail.com>
2021-11-24 22:22:23 -0500

    adding build,pipeline and requirements

--- pipeline.py
+++ pipeline.py
@@ -12,14 +12,38 @@ timestr = time.strftime("%H%M")
 pipeline_name='pipe-{}'.format(timestr)
 compiled_pipe='basic_pipeline_1'
 
-@component
-def op(a: float, b: float) -> float:
-    return a+b
+PROJECT_ID='tron-302502'
+BUCKET_NAME="gs://" + PROJECT_ID + "-bucket"
+IMAGE_URI='gcr.io/tron-302502/kfp/custom:v1'
+PIPELINE_ROOT = f"{BUCKET_NAME}/pipeline_root/"
+REGION = 'us-central1'
+
 
 @pipeline(name="{}".format(pipeline_name), description="A simple intro pipeline",
           pipeline_root='gs://tron-302502-bucket/basic-pipe')
-def basic_pipeline(a: float=468, b: float=864):
-    op_task = op(a,b)
+def pipeline(
+    bq_source: str = "bq://sara-vertex-demos.beans_demo.large_dataset",
+    project: str = PROJECT_ID,
+    gcp_region: str = REGION,
+    container_uri: str = IMAGE_URI,
+    bigquery_destination: str = f"bq://{}".format(PROJECT_ID),
+):
+    dataset_create_op = gcc_aip.TabularDatasetCreateOp(
+        display_name="tabular-beans-dataset",
+        bq_source=bq_source,
+        project=project,
+        location=gcp_region
+    )
+
+    training_op = gcc_aip.CustomContainerTrainingJobRunOp(
+        display_name="pipeline-beans-custom-train",
+        container_uri=container_uri,
+        project=project,
+        dataset=dataset_create_op.outputs["dataset"],
+        staging_bucket=BUCKET_NAME,
+        bigquery_destination=bigquery_destination
+    )
+
     
 if __name__ == '__main__':
     compiler.Compiler().compile(
